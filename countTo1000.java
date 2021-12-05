3b274effbe71b31c057933bb17c299b319fe59b8
Williams Tech <56772579+williams-tech@users.noreply.github.com>
2021-12-05 21:52:41 +0100

    Create countTo1000.java

---
+++ countTo1000.java
@@ -0,0 +1,10 @@
+public class Test {
+    public static void main (String args[]) {
+        int num = 0;
+        int max = 100;
+        while (num != max) {
+            num++;
+            System.out.println(num);
+        }
+    }
+}
